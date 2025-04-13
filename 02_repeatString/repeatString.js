03a51398ccd702fcdf802833801e8331f18fcecc
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 14:50:34 -0500

    02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR"
+    }
+    let res = "";
+    while(num > 0){
+        res += str;
+        num--;
+    }
+    return res;
 };
 
 // Do not edit below this line
