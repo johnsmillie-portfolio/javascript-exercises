29e2f4639d41e67ff9a8d4d1304bd1249d1cf284
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 14:57:28 -0500

    03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const chars = str.split("");
+    let res = "";
+    for(let i = chars.length-1; i >=0; i--){
+        res += chars[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
