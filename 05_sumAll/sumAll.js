d2e503369b0cfb5b5e23d4939899f67a8f3cf90a
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 15:35:14 -0500

    05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    }    
+    if(start < end) {start--;}
+    else{end--;}
+    const x = (end*(end+1))/2;
+    const y = (start*(start+1))/2;
+    return Math.abs(x-y);
 };
 
 // Do not edit below this line
