3fe9b78f71e3a35fdd2a0c81e195fa5815d5a0a1
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 15:19:35 -0500

    04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const res = new Array();
+
+    const set = new Set(args);
+
+    
+
+    for(let i = 0; i < arr.length; i++){
+        if(!set.has(arr[i])){
+            res.push(arr[i])
+        }
+    }
+   
+    return res;
 };
 
 // Do not edit below this line
