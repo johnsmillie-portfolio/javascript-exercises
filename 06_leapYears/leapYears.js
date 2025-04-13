ba077d76f72674c2a8cf55da680d39895b69446e
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 15:47:58 -0500

    06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    isDivisibleBy4 = year % 4 === 0;
+    isDivisibleBy100 = year % 100 === 0;
+    isDivisibleBy400 = year % 400 === 0;
+    return  (isDivisibleBy4 && !isDivisibleBy100) || isDivisibleBy400;
 };
 
 // Do not edit below this line
