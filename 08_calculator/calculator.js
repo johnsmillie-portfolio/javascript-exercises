a6f69c3e75906bdb725591145f206bcbc30c5223
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 21:38:21 -0500

    08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if(nums.length === 0){return 0}
+	return nums.reduce((sum , el) => sum += el)
+};
+;
+const multiply = function(nums) {
+  return nums.reduce((prod, el) => prod *= el)
 };
 
-const multiply = function() {
-
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+  while(num > 1){
+    res *= num--;
+  }
+  return res;
 };
 
 // Do not edit below this line
