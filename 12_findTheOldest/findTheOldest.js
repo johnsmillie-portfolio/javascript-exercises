eec563e2e6926038f17e7144875b89a18df38ced
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 22:36:54 -0500

    12 find oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest;
+    let max = 0;
+    const today = new Date().getFullYear();
+    people.forEach((el) =>{
+        let age = el.hasOwnProperty("yearOfDeath") ? el.yearOfDeath - el.yearOfBirth :
+            today - el.yearOfBirth;
+        if(age > max){
+            max = age;
+            oldest = el;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
