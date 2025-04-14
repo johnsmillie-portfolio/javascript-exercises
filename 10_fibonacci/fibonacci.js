25de0c4c8a4ace48ee4b0fe4d861b3fd63c09cb2
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 22:18:17 -0500

    10 fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(Number.isNaN(num)){return Number(num)}
+    if(num < 0){return "OOPS"}
+    if(num == 0){return 0}
+    if(num <= 2){return 1}
+    let prim = 1;
+    let sec = 1;
+    let x;
+    while(num > 2){
+        x = prim + sec;
+        sec = prim;
+        prim = x;
+        num--;
+    }
+    return prim;
 };
 
 // Do not edit below this line
