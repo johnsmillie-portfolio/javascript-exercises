6061c556e5aa911d2c0cb9e22e1b6489ecf18177
johnsmillie-student <jsmillie@depaul.edu>
2025-04-13 22:05:21 -0500

    09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.replace(/["."|","|"!"|"?"|" "]/g, "").toLowerCase();
+    const chars = word.slice("");
+    let l = 0;
+    let r = chars.length-1;
+    while(l <= r){
+        if(chars[l++] !== chars[r--]){return false;}
+    }
 
+    return true;
 };
 
 // Do not edit below this line
